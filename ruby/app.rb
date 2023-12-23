c80ebb75de9c6a165e8a7ad4b63baedfcf1d817c
degwinthegreat <the.colledge.dropout@gmail.com>
2023-12-23 19:13:45 +0900

    Refactor tag retrieval in app.rb

--- ruby/app.rb
+++ ruby/app.rb
@@ -113,8 +113,14 @@ module Isupipe
         owner_model = tx.xquery('SELECT * FROM users WHERE id = ?', livestream_model.fetch(:user_id)).first
         owner = fill_user_response(tx, owner_model)
 
-        tags = tx.xquery('SELECT * FROM livestream_tags WHERE livestream_id = ?', livestream_model.fetch(:id)).map do |livestream_tag_model|
-          tag_model = tx.xquery('SELECT * FROM tags WHERE id = ?', livestream_tag_model.fetch(:tag_id)).first
+        # Get all livestream_tag_models for this livestream in one query
+        livestream_tag_ids = tx.xquery('SELECT tag_id FROM livestream_tags WHERE livestream_id = ?', livestream_model.fetch(:id), as: :array).map(&:first)
+
+        # Get all tags in one query using WHERE id IN
+        tags_models = tx.xquery('SELECT * FROM tags WHERE id IN (?)', livestream_tag_ids)
+
+        # Map the tags_models to the required format
+        tags = tags_models.map do |tag_model|
           {
             id: tag_model.fetch(:id),
             name: tag_model.fetch(:name),
