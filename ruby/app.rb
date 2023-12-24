b61b1d3f9313faf1e00e4ab28482c146d22fdfa5
degwinthegreat <the.colledge.dropout@gmail.com>
2023-12-24 21:50:26 +0900

    Add created_at column to livecomments table

--- ruby/app.rb
+++ ruby/app.rb
@@ -503,7 +503,7 @@ module Isupipe
           livecomments.livestream_id AS l_livestream_id,
           livecomments.comment AS l_comment,
           livecomments.tip AS l_tip,
-          livecomments.created_at AS l_created_at
+          livecomments.created_at AS l_created_at,
           users.*
         FROM livecomments
         INNER JOIN users ON livecomments.user_id = users.id
