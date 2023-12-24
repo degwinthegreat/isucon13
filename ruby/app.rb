61467a29709c1f17cc9c9a91621f43e09b69f070
degwinthegreat <the.colledge.dropout@gmail.com>
2023-12-24 15:40:52 +0900

    searchのn+1をgithub copilot chatで解消できるか挑戦

--- ruby/app.rb
+++ ruby/app.rb
@@ -324,14 +324,13 @@ module Isupipe
     get '/api/livestream/search' do
       key_tag_name = params[:tag] || ''
 
+      # fix
       livestreams = db_transaction do |tx|
         livestream_models =
           if key_tag_name != ''
             # タグによる取得
             tag_id_list = [ TAGS_BY_NAME[key_tag_name].fetch(:id) ].reject(&:nil?)
-            tx.xquery('SELECT * FROM livestream_tags WHERE tag_id IN (?) ORDER BY livestream_id DESC', tag_id_list).map do |key_tagged_livestream|
-              tx.xquery('SELECT * FROM livestreams WHERE id = ?', key_tagged_livestream.fetch(:livestream_id)).first
-            end
+            tx.xquery('SELECT livestreams.* FROM livestreams INNER JOIN livestream_tags ON livestreams.id = livestream_tags.livestream_id WHERE livestream_tags.tag_id = ? ORDER BY livestreams.id DESC', tag_id).to_a
           else
             # 検索条件なし
             query = 'SELECT * FROM livestreams ORDER BY id DESC'
